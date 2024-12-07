a17710e01d0c8c3f015ead69f3f09f9da468091f
Zhabrikoff <ezhabrikov@gmail.com>
2024-12-07 13:44:25 +0300

    feat: add #author? test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,18 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe '#author?' do
+    let(:author) { create(:user) }
+    let(:user) { create(:user) }
+    let(:question) { create(:question, user: author) }
+
+    it 'if user is author' do
+      expect(author.author?(question)).to be_truthy
+    end
+
+    it 'if user isn\'t author' do
+      expect(user.author?(question)).to be_falsey
+    end
+  end
 end
